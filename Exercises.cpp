14f46340009546b439a4872f7ef9271154d3d7d9
oani <callmeoani@gmail.com>
2023-12-27 04:31:23 +0100

    examples on namespaces and 'using' declarations & directives

--- Exercises.cpp
+++ Exercises.cpp
@@ -3,150 +3,59 @@
 
 #include "std_lib_facilities.h"
 
-class Token {
-public:
-    char kind;
-    double value;
-};
+//void swap_v(int a, int b) { 
+//    int temp; 
+//    temp = a;
+//    a = b; 
+//    b = temp; 
+//}
+//void swap_r(int& a, int& b) { int temp; temp = a; a = b; b = temp; }
+//void swap_cr(const int& a, const int& b) { int temp; temp = a; a = b; b = temp; }
 
-class Token_stream {
-public:
-    Token get();
-    void putback(Token t);
-private:
-    bool full {false}; //is there a token in the buffer?
-    Token buffer; // here is where we put a token put back using putback()
-};
 
-void Token_stream::putback(Token t) {
-    if (full) error("putback() into a full buffer");
-    buffer = t; //copy t to buffer
-    full = true; //buffer is now full
+namespace X {
+    int var = 0;
+    void print() {
+        cout << "X's var: " << var << '\n';
+    }
 }
-
-Token Token_stream::get() {
-    if (full) { // do we already have a Token ready?
-        full = false; // remove Token from buffer
-        return buffer; 
+namespace Y {
+    int var = 0;
+    void print() {
+        cout << "Y's var: " << var << '\n';
     }
-
-    char ch;
-    cin >> ch; // >> skips whitespaces
-
-    switch (ch) {
-    case ';': // ';' for print
-    case 'q': // 'q' for quit
-    case '(': case ')': case '+': case '-': case '*': case '/':
-        return Token{ ch }; // let each character represent itself
-
-    case '.':
-    case '0':case '1': case '2': case '3': case '4': 
-    case '5': case '6': case '7': case '8': case '9':
-    {
-        cin.putback(ch); //put digit back into the input stream;
-        double val;
-        cin >> val; // read a floating-point number
-        return Token{ '8', val }; // let '8' represent "a number"
-    }
-    default:
-        error("Bad token");
-    }
-
-
 }
-
-Token_stream ts;
-
-double expression();
-
-double primary() {
-    Token t = ts.get();
-    while(true) {
-        switch (t.kind) {
-        case '(':
-        {
-            double d = expression();
-            t = ts.get();
-            if (t.kind != ')') error("')' expected!");
-            return d;
-        }
-        case '8':
-            return t.value;
-        default:
-            error("Primary expected!");
-        }
+namespace Z {
+    int var= 0;
+    void print() {
+        cout << "Z's var: " << var << '\n';
     }
 }
 
-double term() {
-    double left = primary(); //look for primary
-    Token t = ts.get();
-
-    while (true) {
-        switch (t.kind) {
-        case '*':
-            left *= primary();
-            t = ts.get();
-            break;
-        case '/':
-        {
-            double primary_value = primary(); // because of this, everything is placed inside a block
-            if (primary_value == 0) error("Division by 0!");
-            left /= primary_value;
-            t = ts.get();
-            break;
-        }
-        default:
-            ts.putback(t);
-            return left;
-        }
-    }
-
-}
-
-double expression() {
-    double left = term(); //look for a term
-    Token t = ts.get(); //get the next token
-
-    while (true) {
-        switch (t.kind) {
-        case '+':
-            left += term(); //evaluate Term and add
-            t = ts.get(); // get the next token
-            break;
-        case '-':
-            left -= term();
-            t = ts.get();
-            break;
-        default:
-            ts.putback(t);
-            return left;
-        }
-    }
-}
 
 int main()
 try {
-    cout << "Enter some values: \n";
+    X::var = 7;
+    X::print();
 
-    double val = 0;
-    
-    while (cin) { //as long as we are getting input through cin
-        Token t = ts.get();
+    using namespace Y;
+    var = 9;
+    print();
 
-        if (t.kind == 'q') break; //'q' for quit
-        if (t.kind == ';') {
-            cout << "= " << val << '\n';
-        }
-        else {
-            ts.putback(t);
-            val = expression();
-        }
-        //keep_window_open();
+    {
+        using Z::var;
+        using Z::print;
+
+        var = 11;
+        print();
     }
+
+    print();
+    X::print();
+
+
     
 
-    
 
 
 
@@ -168,17 +77,23 @@ try {
 
 
 
-
-
+    //int x = 7;
+    //int y = 9;
+    //swap_r(7, 9);
+    //cout << "x: " << x << " & y: " << y << '\n';
 }
 catch (exception& e) {
     cerr << "Error: " << e.what() << '\n';
-    keep_window_open();
+    keep_window_open("~~");
+    //for (char ch; cin >> ch; ) { //keep reading until we find a ~
+    //    if (ch == '~') return 1;
+    //}
+
     return 1;
 }
 catch (...) {
     cerr << "Exception error\n";
-    keep_window_open();
+    keep_window_open("~~");
     return 2;
 }
 
